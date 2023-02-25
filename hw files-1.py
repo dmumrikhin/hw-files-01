ef3da6e24f85ccdaf95d84e5ad00c8e5d08016f2
dmitry umrikhin <u-dv@yandex.ru>
2023-02-25 18:07:36 +0300

    task 2 commit

--- hw files-1.py
+++ hw files-1.py
@@ -11,10 +11,32 @@ with open('recipes.txt') as file:
             ingredient_name, quantity, measure = emp.split(' | ')
             ingredient.append(
                 {'ingredient_name': ingredient_name,
-                 'quantity': quantity,
+                 'quantity': int(quantity),
                  'measure': measure}
             )
         cook_book[dish_name] = ingredient
         file.readline()
 
-pprint(cook_book) #, sort_dicts=False)
+# pprint(cook_book) #, sort_dicts=False)
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_dict = {}
+    for dish in dishes:
+        for key, value in cook_book.items():
+            if key == dish:
+                for item in value:
+                    if item['ingredient_name'] in shop_dict:
+                        shop_dict[item['ingredient_name']]['quantity'] += item['quantity'
+                            ] * person_count
+                    else:
+                        ingredient_quantity = {}
+                        ingredient_quantity['measure'] = item['measure']
+                        ingredient_quantity['quantity'] = item['quantity'] * person_count
+                        shop_dict[item['ingredient_name']] = ingredient_quantity
+    return(shop_dict) 
+
+
+pprint(get_shop_list_by_dishes(['Омлет', 'Фахитос'], 2))
+
+
+
